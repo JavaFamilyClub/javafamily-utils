6be6f66631dc610562208d60248078122cecb8f7
DreamLi <18829346477@163.com>
2020-04-27 23:00:24 +0800

    init project

---
+++ src/test/java/org/javafamily/util/HttpUtilTests.java
@@ -0,0 +1,25 @@
+package org.javafamily.util;
+
+import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpUtilTests {
+
+   @Test
+   @Disabled("Query count limit for everyday, so disable this test.")
+   public void testGet() throws Exception {
+      String api = "/Weather/Query";
+      Map<String, String> params = new HashMap<>();
+
+      params.put("cityname", "西安");
+
+      String result = HttpUtil.get(api, params);
+
+      System.out.println(result);
+
+      Assertions.assertNotNull(result);
+   }
+
+}
