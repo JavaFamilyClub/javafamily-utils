eea96681914f0e01bf4c7dfc2858ef0ccc1e2f72
DreamLi <18829346477@163.com>
2020-05-08 02:03:28 +0800

    add json utils

---
+++ src/main/java/org/javafamily/util/JsonUtils.java
@@ -0,0 +1,36 @@
+package org.javafamily.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.Serializable;
+
+public final class JsonUtils {
+
+   public static String objectToJson(Serializable obj)
+      throws JsonProcessingException
+   {
+      if(obj == null) {
+         return null;
+      }
+
+      ObjectMapper mapper = new ObjectMapper();
+
+      return mapper.writeValueAsString(obj);
+   }
+
+   public static <T> T jsonToObject(String json, Class<T> clazz)
+      throws JsonProcessingException
+   {
+      if(json == null) { // skip empty string, then return empty object
+         return null;
+      }
+
+      ObjectMapper mapper = new ObjectMapper();
+
+      return mapper.readValue(json, clazz);
+   }
+
+   private JsonUtils() {
+   }
+}
