e8916d2daf63ce59846c84a1a2ade423b279df2b
李帅 <lishuai@mlogcn.com>
2022-06-10 12:04:10 +0800

    add check pdf valid method。

---
+++ javafamily-utils-pdf-itext/src/main/java/club/javafamily/utils/pdf/PdfUtils.java
@@ -0,0 +1,51 @@
+package club.javafamily.utils.pdf;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Jack Li
+ * @date 2022/6/10 上午11:42
+ * @description
+ */
+@Slf4j
+public class PdfUtils {
+
+   private static boolean check(InputStream in) {
+      boolean valid = false;
+
+      int pageCount = 0;
+
+      PdfDocument pdfDocument = null;
+
+      try {
+         PdfReader pdfReader = new PdfReader(in);
+
+         pdfDocument = new PdfDocument(pdfReader);
+         pageCount = pdfDocument.getNumberOfPages();
+
+         if(pageCount != 0) {
+            valid = true;
+         }
+
+//         final PdfPage lastPage = pdfDocument.getFirstPage();
+//         String textFromPdfFilePageOne
+//            = PdfTextExtractor.getTextFromPage(lastPage);
+
+         pdfDocument.close();
+      } catch (IOException e) {
+         log.debug("PDF check failed!", e);
+
+         if(pdfDocument != null) {
+            pdfDocument.close();
+         }
+      }
+
+      return valid;
+   }
+
+}
